f75c057cbd8420eb5b6b003cf416dba21b15fe1f
Shashvat <shashvatpatel2803@gmail.com>
2024-03-15 00:00:29 +0530

    Added text-styling

--- src/Carousel/Carousel.jsx
+++ src/Carousel/Carousel.jsx
@@ -6,16 +6,15 @@ export const Carousel = () => {
     <div className='carousel'>
         <div className='carousel__slide'>
             <div className='overlay'></div>
-            <img src='https://image.tmdb.org/t/p/w1280/fGe1ej335XbqN1j9teoDpofpbLX.jpg' alt='Movie' />
+            <img src='http://image.tmdb.org/t/p/w1280/8ZTVqvKDQ8emSGUEMjsS4yHAwrp.jpg' alt='Movie' />
             {/* <img className="image2" src='https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SY1000_SX677_AL_.jpg' alt="Movie" />
             <img className='image1' src='https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SY1000_SX677_AL_.jpg' alt="Movie" /> */}
-            
             <div className='carousel__content'>
-                <h1 className='carousel__title'>The Shawshank Redemption</h1>
+                <h1 className='carousel__title'>Inception</h1>
                 <div className='carousel__specifics'>
-                    <p>Date &nbsp;</p>
-                    <p>Imdb Rating &nbsp;</p>
-                    <p>Genre &nbsp;</p>
+                    <p>2023/2/25 &nbsp;</p>
+                    <p>8.3 &nbsp;</p>
+                    <p>Action | Thriller &nbsp;</p>
                 </div>
                 <div className='carousel__description'>Two imprisoned lorem ipsum lorem ipsum plsfh asjasjdas  hasjd ja sjd jasd jajsdasjdjasd j jasdjad  asda asdasd sadasd asdasd asdadasd sadasdas dasdasdhghjkh  </div>
             </div>
