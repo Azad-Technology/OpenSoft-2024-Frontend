dead8bf2dabb489c8bdcf00220b618cf258c3e62
Kalidas <110555858+DesolatedHope@users.noreply.github.com>
2024-03-14 22:41:59 +0530

    Updated CSS, tested blurred image variant

--- src/Carousel/Carousel.jsx
+++ src/Carousel/Carousel.jsx
@@ -3,20 +3,23 @@ import './Carousel.css'
 
 export const Carousel = () => {
   return (
-    <div className='Carousel'>
-        <div className='Carousel_slide'>
+    <div className='carousel'>
+        <div className='carousel__slide'>
             <div className='overlay'></div>
             <img src='https://image.tmdb.org/t/p/w1280/fGe1ej335XbqN1j9teoDpofpbLX.jpg' alt='Movie' />
-            <div className='Carousel_content'>
-                <h1 className='Carousel__title'>The Shawshank Redemption</h1>
-                <div className='Carousel_Specifics'>
+            {/* <img className="image2" src='https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SY1000_SX677_AL_.jpg' alt="Movie" />
+            <img className='image1' src='https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SY1000_SX677_AL_.jpg' alt="Movie" /> */}
+            
+            <div className='carousel__content'>
+                <h1 className='carousel__title'>The Shawshank Redemption</h1>
+                <div className='carousel__specifics'>
                     <p>Date &nbsp;</p>
                     <p>Imdb Rating &nbsp;</p>
                     <p>Genre &nbsp;</p>
                 </div>
-                <div className='Carousel__description'>Two imprisoned lorem ipsum lorem ipsum plsfh asjasjdas  hasjd ja sjd jasd jajsdasjdjasd j jasdjad  asda asdasd sadasd asdasd asdadasd sadasdas dasdasdhghjkh  </div>
+                <div className='carousel__description'>Two imprisoned lorem ipsum lorem ipsum plsfh asjasjdas  hasjd ja sjd jasd jajsdasjdjasd j jasdjad  asda asdasd sadasd asdasd asdadasd sadasdas dasdasdhghjkh  </div>
             </div>
-                {/* <div className='Carousel_sideslide'>
+                {/* <div className='carousel__sideslide'>
                 <img src='https://image.tmdb.org/t/p/w154/wShcJSKMFg1Dy1yq7kEZuay6pLS.jpg' alt='Movie' />
                 </div> */}
         </div>
