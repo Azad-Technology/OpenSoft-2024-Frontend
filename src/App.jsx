7e99037b63993212d49bb74fa1077cc847307639
Anuragp22 <anuragp2003b@gmail.com>
2024-03-17 02:05:43 +0530

    adding search

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import './App.css'
-import MoreLikeThis from './components/MoreLikeThis/MoreLikeThis'
-import MoviePage from './components/moviePage/MoviePage'
 import SearchPage from './components/SearchPage/SearchPage'
 function App() {
 
   return (
     <>
-      {/* <MoviePage />
-      <MoreLikeThis /> */}
       <SearchPage />
     </>
   )
