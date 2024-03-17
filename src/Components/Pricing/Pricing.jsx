5cf9005a946d9a4ff1b128b8ca6b49c75a3d709a
Kalidas <110555858+DesolatedHope@users.noreply.github.com>
2024-03-17 22:43:16 +0530

    data

--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -3,48 +3,7 @@ import { PricingCard } from './PricingCard'
 import styles from './Pricing.module.css'
 
 export const Pricing = () => {
-    const plans = [
-        {
-            name: "Free",
-            price: 12,
-            features: [
-                "Curabitur faucibus",
-                "massa ut pretium maximus",
-                "Sed posuere nisi",
-                "Pellentesque eu nibh et neque",
-                "Suspendisse a leo",
-                "Praesent quis venenatis ipsum",
-                "Duis non diam vel tortor",
-
-            ],
-        },
-        {
-            name: "Standard",
-            price: 35,
-            features: [
-                "Curabitur faucibus",
-                "massa ut pretium maximus",
-                "Sed posuere nisi",
-                "Pellentesque eu nibh et neque",
-                "Suspendisse a leo",
-                "Praesent quis venenatis ipsum",
-                "Duis non diam vel tortor",
-            ],
-        },
-        {
-            name: "Premium",
-            price: 60,
-            features: [
-                "Curabitur faucibus",
-                "massa ut pretium maximus",
-                "Sed posuere nisi",
-                "Pellentesque eu nibh et neque",
-                "Suspendisse a leo",
-                "Praesent quis venenatis ipsum",
-                "Duis non diam vel tortor",
-            ],
-        },
-    ];
+    
   return (
     <div className={`${styles.pricing}`}>
         {plans.map((plan, index) => {
--- src/Components/Pricing/PricingCard.jsx
+++ src/Components/Pricing/PricingCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './PricingCard.module.css'
+import styles from './styles.module.css'
 
 export const PricingCard = ({plan}) => {
   return (
