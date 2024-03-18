53cb454106cd0d909e1d6d092666211e1b22ac77
Anuragp22 <anuragp2003b@gmail.com>
2024-03-18 23:18:11 +0530

    Removed home for searchPage, few changes in css

--- src/App.jsx
+++ src/App.jsx
@@ -112,9 +112,9 @@ const App = () => {
         <Route path="/search/:searchTerm" element={
           <>
           <Navbar />
-          <div className='home'>
+          {/* <div className='home'> */}
           <SearchPage />
-          </div>
+          {/* </div> */}
           </>
         } />
         <Route path='/pricing' element={
