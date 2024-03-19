a007103d34b37caa0748ea8bd1fb3a0e59aa9f4a
Shashvat <shashvatpatel2803@gmail.com>
2024-03-20 00:17:55 +0530

    Feature Added: Video player Modal

--- src/Components/MoviePlayback/Player.jsx
+++ src/Components/MoviePlayback/Player.jsx
@@ -2,17 +2,38 @@ import '@vidstack/react/player/styles/default/theme.css';
 import '@vidstack/react/player/styles/default/layouts/video.css';
 import { MediaPlayer, MediaProvider } from '@vidstack/react';
 import { defaultLayoutIcons, DefaultVideoLayout } from '@vidstack/react/player/layouts/default';
+import React, { useState } from 'react';
+import './styles.css';
 
+function Modal({ onClose }) {
+  return (
+    <div className="modal-overlay">
+      <div className="modal">
+        {/* Video container */}
+        <div className="video-container">
+          <div className='video'>
+            <MediaPlayer title="Dune" src="/manifests/dune_master.m3u8">
+              <MediaProvider />
+              <DefaultVideoLayout icons={defaultLayoutIcons} />
+            </MediaPlayer>
+          </div>
+        </div>
+        {/* Close button */}
+        <button className="close-button" onClick={onClose}>X</button>
+      </div>
+    </div>
+  );
+}
 function Player() {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <>
+      <div className="app">
+        <button onClick={() => setShowModal(true)}>Open Modal</button>
+        {showModal && <Modal onClose={() => setShowModal(false)} />}
+      </div>
+    </>
+  )
+}
 
-    return (
-      <>
-        <MediaPlayer title="Dune" src="/manifests/dune_master.m3u8">
-          <MediaProvider />
-          <DefaultVideoLayout  icons={defaultLayoutIcons} />
-        </MediaPlayer>
-      </>
-    )
-  }
-  
-  export default Player
+export default Player
