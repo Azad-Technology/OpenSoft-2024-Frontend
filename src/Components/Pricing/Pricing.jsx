211a143a2cc5791b23601abe3281dcb12a526b82
Anuragp22 <anuragp2003b@gmail.com>
2024-03-20 00:12:23 +0530

    Pricing Links Added

--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import bgBottom from "../../assets/bg-bottom.svg";
 import bgTop from "../../assets/bg-top.svg";
 import styles from "./styles.module.css";
+import { Link } from 'react-router-dom';
 function Pricing() {
   const [annually, setAnnually] = useState(false);
   return (
@@ -18,11 +19,11 @@ function Pricing() {
 
           <article className={styles.pricingCard}>
             <h2>Basic</h2>
-            
-              <h3>
-                <span>$</span> 19.99
-              </h3>
-            
+
+            <h3>
+              <span>$</span> 19.99
+            </h3>
+
             <ul>
               <li>
                 500 Searches every Month
@@ -34,17 +35,15 @@ function Pricing() {
                 Send up to 3 GB
               </li>
             </ul>
-            <button className={styles.pricingCard_button}>
-              Purchase
-            </button>
+            <Link target="_blank" to="paisawala.lemonsqueezy.com"><button className={styles.pricingCard_button}>Purchase</button></Link>
           </article>
 
           <article className={`${styles.pricingCard} ${styles.featured}`}>
             <h2>Standard</h2>
-              <h3>
-                <span>$</span> 24.99
-              </h3>
-            
+            <h3>
+              <span>$</span> 24.99
+            </h3>
+
             <ul>
               <li>
                 1 TB Storage
@@ -56,16 +55,16 @@ function Pricing() {
                 Send up to 10 GB
               </li>
             </ul>
-            <button>
-              Purchase
-            </button>
+
+            <Link target="_blank" to="https://paisawala.lemonsqueezy.com/checkout/buy/d7accfc5-fe92-41d3-a155-82e72dfcfd90?embed=1"><button className={styles.pricingCard_button}>Purchase</button></Link>
+
           </article>
 
           <article className={`${styles.pricingCard} ${styles.lastCard}`}>
             <h2>Premium</h2>
-              <h3>
-                <span>$</span> 39.99
-              </h3>
+            <h3>
+              <span>$</span> 39.99
+            </h3>
             <ul>
               <li>
                 500 GB Storage
@@ -77,9 +76,11 @@ function Pricing() {
                 Send up to 3 GB
               </li>
             </ul>
-            <button>
-              Purchase
-            </button>
+
+            <Link target="_blank" to="https://paisawala.lemonsqueezy.com/checkout/buy/1840ab67-0984-4d07-98e3-7439cfa644d4?embed=1">
+              <button className={styles.pricingCard_button}>Purchase</button>
+            </Link>
+
           </article>
         </div>
 
