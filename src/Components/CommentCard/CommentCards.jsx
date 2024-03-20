41561418185f5bcdc6e0c88925331beeb6d0f3e0
Kalidas <110555858+DesolatedHope@users.noreply.github.com>
2024-03-20 23:50:32 +0530

    Added margin in slider

--- src/Components/CommentCard/CommentCards.jsx
+++ src/Components/CommentCard/CommentCards.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import CommentCard from './CommentCard';
 import './CommentCard.css';
+import userIcon from '../../assets/userIcon.png'
 
 function CommentCards() {
   const commentsData = [
-    { username: "John Doe", profilePic: "https://example.com/profile.jpg", comment: "This is a great website!", movie: "Movie Title 1", link: "/movie-page-1" },
-    { username: "Jane Smith", profilePic: "https://example.com/profile2.jpg", comment: "I love the design!", movie: "Movie Title 2", link: "/movie-page-2" },
-    { username: "Alice", profilePic: "https://example.com/profile3.jpg", comment: "Nice work!", movie: "Movie Title 3", link: "/movie-page-3" }
+    { username: "John Doe", profilePic: userIcon, comment: "This is a great website!", movie: "Movie Title 1", link: "/movie-page-1" },
+    { username: "Jane Smith", profilePic: userIcon, comment: "I love the design!", movie: "Movie Title 2", link: "/movie-page-2" },
+    { username: "Alice", profilePic: userIcon, comment: "Nice work!", movie: "Movie Title 3", link: "/movie-page-3" }
     // Add more comment objects as needed
   ];
 
