9cd2a5ea6439c3418d79972173e3b912742529b2
arghyadhali123 <dhali.arghya2016@gmail.com>
2024-03-21 23:50:43 +0530

    Comment Card on Different Page

--- src/Components/CommentCard/CommentCard.jsx
+++ src/Components/CommentCard/CommentCard.jsx
@@ -1,13 +1,65 @@
 import React from 'react';
-import './CommentCard.css';
-function CommentCard({ username, profilePic, comment, movie, link }) {
+import styles from './CommentCard.module.css';
+
+function truncateComment(comment, maxLength) {
+  if (comment.length <= maxLength) {
+    return comment;
+  }
+  // Split the comment into words
+  const words = comment.split(' ');
+  // Concatenate the first few words and add "..."
+  const truncatedComment = words.slice(0, 5).join(' ') + '...';
+  return truncatedComment;
+}
+
+function timeSince(date) {
+  const seconds = Math.floor((new Date() - date) / 1000);
+
+  let interval = Math.floor(seconds / 31536000);
+  if (interval > 1) {
+    return `${interval} years ago`;
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return `${interval} months ago`;
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return `${interval} days ago`;
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return `${interval} hours ago`;
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return `${interval} minutes ago`;
+  }
+  return `${Math.floor(seconds)} seconds ago`;
+}
+
+
+function CommentCard({ username, profilePic, comment, movie, link, timestamp }) {
+  const commentTime = new Date(timestamp);
+  const timeAgo = timeSince(commentTime);
+  
+  // const isValidProfilePic = profilePic && typeof profilePic === 'string' && profilePic.trim() !== '';
+  // const defaultProfilePic = 'https://i.pinimg.com/564x/83/bc/8b/83bc8b88cf6bc4b4e04d153a418cde62.jpg';
+
+  // const updatedProfilePic = isValidProfilePic ? profilePic : (defaultProfilePic && typeof defaultProfilePic === 'string' && defaultProfilePic.trim() !== '') ? defaultProfilePic : 'fallback.jpg';
+  
+
   return (
-    <div className="comment-card">
-      <img src={profilePic} alt={username} className="profile-pic" />
-      <div className="comment-details">
-        <h3 className="card-title">{username}</h3>
-        <p className="card-comment">{comment}</p>
-        <a href={link} className="card-movie">{movie}</a>
+    <div className={styles.commentCard}>
+      <div className={styles.title}>
+        <img src={profilePic} alt="" className={styles.profilePic} />
+        <h3 className={styles.username}>{username}</h3>
+      </div>
+      
+      <div className={styles.commentDetails}>
+        <p className={styles.cardComment}>{truncateComment(comment, 10)}</p>
+        <div className={styles.cardTime}>{timeAgo}</div>
+        <a href={link} className={styles.cardMovie}>{movie}</a>
       </div>
     </div>
   );
