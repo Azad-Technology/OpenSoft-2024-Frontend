3c442e40290aa41a6fb279fd0f98ef9373d327cf
Kalidas <110555858+DesolatedHope@users.noreply.github.com>
2024-03-22 02:53:36 +0530

    Adding imdb icon

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import styles from "./Card.module.css";
+import imdbIcon from '../../assets/imdb-icon.svg';
 import { useNavigate } from "react-router-dom";
 
 const Card = ({ movies }) => {
