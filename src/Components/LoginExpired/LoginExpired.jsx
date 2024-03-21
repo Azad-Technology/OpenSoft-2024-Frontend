8b92e666fd684b9e860322455602af3740302723
Kalidas <110555858+DesolatedHope@users.noreply.github.com>
2024-03-21 19:21:56 +0530

    Added Login Expired page

---
+++ src/Components/LoginExpired/LoginExpired.jsx
@@ -0,0 +1,19 @@
+import styles from "./LoginExpired.module.css";
+import { useNavigate } from 'react-router-dom';
+
+const LoginExpired = ()=>{
+    const navigate=useNavigate();
+
+    return(
+        <>
+        <div className={styles.container}>
+            <div className={styles.fourOfour}>LOGIN EXPIRED</div>
+            <div className={styles.heading}>Oops!</div>
+            <div className={styles.subheading}>Sorry but it seems that you are not logged in currently, kindly log in to view this page.</div>
+            <div className={styles.LoginExpired} onClick={()=>{navigate('/login')}}>Log In</div>
+        </div>
+        </>
+    )
+}
+
+export default LoginExpired;
