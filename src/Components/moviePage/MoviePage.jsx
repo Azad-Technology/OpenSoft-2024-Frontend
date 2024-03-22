617e7fa8c880c8987bf57792663cd3978082ddaf
Kalidas <110555858+DesolatedHope@users.noreply.github.com>
2024-03-22 23:17:39 +0530

    Shifted Watchlist to it's own component

--- src/Components/moviePage/MoviePage.jsx
+++ src/Components/moviePage/MoviePage.jsx
@@ -14,6 +14,7 @@ import { useNavigate } from "react-router-dom";
 import watchlistoff from './../../assets/watchlistoff.svg';
 import watchliston from './../../assets/watchliston.svg';
 import Loader from './../Loader/Loader.jsx';
+import WatchListModal from "./WatchListModal.jsx";
 // import './styles.css';
 // import { Time } from "@vidstack/react";
 
@@ -39,68 +40,6 @@ function Modal({ onClose }) {
 }
 import MoreLikeThis from "./MoreLikeThis/MoreLikeThis";
 
-const WatchListModal = ({ onClose, movieID, token }) => {
-    const [watchlistName, setWatchListName] = useState('');
-    const [errorMsg, setErrorMsg] = useState('');
-    const createWatchList = async () => {
-        console.log(watchlistName)
-        if(watchlistName === ''){
-            setErrorMsg('Please enter a name');
-            return;
-        }
-        setErrorMsg('');
-        try{
-            const response = await instance.post(`/add_watchlist/${watchlistName}`, {
-                headers:{
-                    Authorization: `Bearer ${token}`
-                }
-            });
-            console.log(response);
-
-        } catch(err){
-            console.log(err);
-        }
-    }
-
-    return (
-    <div className={styles.watchlist_modal_overlay}>
-        <div className={styles.watchlist_modal}>
-            <div className={styles.watchlist_modal_content}>
-            <div className={styles.watchlist_modal_heading}>Add to Watchlist</div>
-
-            <div className={styles.watchlist_modal_section}>
-                <div className={styles.watchlist_modal_section_heading}>Create New Watchlist</div>
-                <div className={styles.watchlist_create}>
-                <input
-                type="text"
-                placeholder="Enter watchlist name"
-                className={styles.watchlist_modal_input}
-                value={watchlistName}
-                onChange={(e) => setWatchListName(e.target.value)}
-                />
-                <button className={styles.watchlist_modal_button} onClick={createWatchList}>Create</button>
-                </div>
-                {errorMsg && <div className={styles.watchlist_error}>{errorMsg}</div>}
-            </div>
-            <div className={styles.watchlist_divider}></div>
-            <div className={styles.watchlist_modal_section}>
-                <div className={styles.watchlist_modal_section_heading}>Add to Existing</div>
-                <div className={styles.watchlist_modal_buttons}>
-                <button className={styles.watchlist_modal_button} >Watchlist 1</button>
-                <button className={styles.watchlist_modal_button}>Watchlist 2</button>
-                <button className={styles.watchlist_modal_button}>Watchlist 3</button>
-                <button className={styles.watchlist_modal_button}>Watchlist 4</button>
-                <button className={styles.watchlist_modal_button}>Watchlist 4</button>
-                <button className={styles.watchlist_modal_button}>Watchlist 4</button>
-                <button className={styles.watchlist_modal_button}>Watchlist 4</button>
-                </div>
-            </div>
-            </div>
-            <button className={styles.watchlist_close_button} onClick={onClose}>X</button>
-        </div>
-    </div>
-    );
-}
 
 const MoviePage = () => {
 
