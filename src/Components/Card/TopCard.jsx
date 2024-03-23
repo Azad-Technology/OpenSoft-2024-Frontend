df2d390b9cd404d9bd1cedba2da13e9c514dec1e
Harsh Vardhan Gupta <harsh247@gmail.com>
2024-03-24 00:19:21 +0530

    added favourite functionality

--- src/Components/Card/TopCard.jsx
+++ src/Components/Card/TopCard.jsx
@@ -1,22 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from "./Card.module.css";
 import { useNavigate } from "react-router-dom";
+import instance from "../../axios.jsx";
+import { useStateValue } from "../../MyContexts/StateProvider.jsx";
+
 
 const TopCard = ({ movies, val, lenght }) => {
+  const [{user, token},dispatch]=useStateValue();
+
     const [premium, setPremium] = useState(movies?.imdb.rating>=8);
     const [like, setlike] = useState(false);
     const [value, setvalue] = useState("-o");
     const openHeart = (event) => {
-        event.stopPropagation();
-        if (value === "" && like) {
-            setvalue("-o"); setlike(false);
-        } else {
-            setvalue(""); setlike(true);
-        }
+        if(like){
+            setlike(false);
+          }else{
+            setlike(true);
+          }
+          event.stopPropagation();
+          addFavouriteRequest();
     };
     const handlehover = (event) => {
         console.log(movies)
-        const particularCard = document.getElementById(`${movies._id}`);
+        const particularCard = document.getElementById(`${movies?._id}`);
         if (val === length - 1) {
             particularCard.style.transformOrigin = "right";
         }
@@ -24,6 +30,31 @@ const TopCard = ({ movies, val, lenght }) => {
             particularCard.style.transformOrigin = "left";
         }
     }
+
+    const addFavouriteRequest = async(e)=>{
+        try{
+          const response=await instance.patch(`/add_favourite/${movies?._id}`,null, 
+          {
+            headers:{
+              'Content-Type':'application/json',
+              Authorization: `Bearer ${token}`
+            }
+          });
+          if(!like){
+            dispatch({type:"ADD_FAV", movie:movies});
+          }else{
+            dispatch({type:"REM_FAV", movie:movies});
+          }
+        }
+        catch(err){
+          console.log(err);
+        }
+      }
+      
+      useEffect(()=>{
+        setlike(user?.fav.some(movie=>movie?._id===movies?._id));
+      }, [movies, user])
+
     const navigate = useNavigate();
     return (
         <div className={styles.contain}>
@@ -41,12 +72,15 @@ const TopCard = ({ movies, val, lenght }) => {
                 </div>
                 <div>
                     <div className={styles.icon}>
-                        <i
-                            class={`fa fa-heart${value}`}
-                            aria-hidden="true"
-                            onClick={openHeart}
-                            style={{ color: like ? "red" : "white" }}
-                        ></i>
+                    {like?<i
+              class={`fa fa-heart`}
+              aria-hidden="true"
+              onClick={openHeart}
+            ></i>:<i
+            class={`fa fa-heart-o`}
+            aria-hidden="true"
+            onClick={openHeart}
+          ></i>}
                     </div>
                     <div className={styles.premium}>
             {movies && premium && <i
