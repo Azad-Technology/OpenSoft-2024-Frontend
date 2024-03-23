4eb654a62af986496ecfbf79fd70af67bdd86525
Shashvat <shashvatpatel2803@gmail.com>
2024-03-24 03:14:14 +0530

    Feature: Added Search Functionality in Alan AI

--- src/Components/Alan.jsx
+++ src/Components/Alan.jsx
@@ -5,7 +5,7 @@ const useAlan = (handleFoundGenre) => {
     useEffect(() => {
         alanBtn({
             key: 'a2e30ce08222ef4aac4b4ef40bbcd5ca2e956eca572e1d8b807a3e2338fdd0dc/stage',
-            onCommand: ({ command, genres, genreOrCategory }) => {
+            onCommand: ({ command, genres, genreOrCategory, query }) => {
                 if (command === 'chooseGenre') {
                     const foundGenre = genres.find((g) => g.name.toLowerCase() === genreOrCategory.toLowerCase());
                     handleFoundGenre(foundGenre.name);
@@ -25,6 +25,9 @@ const useAlan = (handleFoundGenre) => {
                 }
                 else if(command === 'buypremium'){
                     window.location.replace('http://localhost:5173/buyPremium');
+                }
+                else if(command === 'search'){
+                    window.location.replace(`http://localhost:5173/search/${query}`);
                 }
             }
         });
