8512ba0943b274259658b69aed4c0dc8835ea218
Harsh Vardhan Gupta <harsh247@gmail.com>
2024-03-29 03:13:46 +0530

    ran prettier

--- src/Components/Watchlists/AllWatchlists.jsx
+++ src/Components/Watchlists/AllWatchlists.jsx
@@ -23,12 +23,11 @@ export const AllWatchlists = () => {
     <>
       {watchlists && watchlists.length > 0 && (
         <>
-          
           <div className={styles.slider}>
             <div className={styles.container}>
               <div className={styles.slider__movies} ref={scrollableDivRef}>
                 {watchlists.map((watchlist, index) => (
-                  <WatchListCards key={index} isOdd={index%2} name={watchlist.name} id={watchlist._id} />
+                  <WatchListCards key={index} isOdd={index % 2} name={watchlist.name} id={watchlist._id} />
                 ))}
               </div>
             </div>
@@ -36,7 +35,34 @@ export const AllWatchlists = () => {
         </>
       )}
 
-      {(!watchlists || watchlists.length === 0) &&  <div className={styles.favorites_card}><div className={styles.nothingToShow}><div> <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M15 18.5L20 13.5M20 18.5L15 13.5" stroke="#ff2e2e" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path opacity="0.5" d="M21 6L3 6" stroke="#ff2e2e" stroke-width="1.5" stroke-linecap="round"></path> <path opacity="0.5" d="M21 10L3 10" stroke="#ff2e2e" stroke-width="1.5" stroke-linecap="round"></path> <path opacity="0.5" d="M11 14L3 14" stroke="#ff2e2e" stroke-width="1.5" stroke-linecap="round"></path> <path opacity="0.5" d="M11 18H3" stroke="#ff2e2e" stroke-width="1.5" stroke-linecap="round"></path> </g></svg></div><div>No watchlist created.</div></div></div>}
+      {(!watchlists || watchlists.length === 0) && (
+        <div className={styles.favorites_card}>
+          <div className={styles.nothingToShow}>
+            <div>
+              {" "}
+              <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <path
+                    d="M15 18.5L20 13.5M20 18.5L15 13.5"
+                    stroke="#ff2e2e"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>{" "}
+                  <path opacity="0.5" d="M21 6L3 6" stroke="#ff2e2e" stroke-width="1.5" stroke-linecap="round"></path>{" "}
+                  <path opacity="0.5" d="M21 10L3 10" stroke="#ff2e2e" stroke-width="1.5" stroke-linecap="round"></path>{" "}
+                  <path opacity="0.5" d="M11 14L3 14" stroke="#ff2e2e" stroke-width="1.5" stroke-linecap="round"></path>{" "}
+                  <path opacity="0.5" d="M11 18H3" stroke="#ff2e2e" stroke-width="1.5" stroke-linecap="round"></path>{" "}
+                </g>
+              </svg>
+            </div>
+            <div>No watchlist created.</div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
--- src/Components/Watchlists/WatchListCards.jsx
+++ src/Components/Watchlists/WatchListCards.jsx
@@ -15,12 +15,56 @@ export const WatchListCards = ({name, id, isOdd}) => {
   let rcolor = "#" + z1 + x + "00";
   return (
     <div className={styles.cardcontainer}>
-      {!isOdd?<div className={styles.card} style={{backgroundImage: "linear-gradient(to right, #26222266, #ffffff51)",}} onClick={() => navigate("/watchlist/" + id)}>
-        <span><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M11 14L3 14" stroke="#fe2a2a" stroke-width="1.5" stroke-linecap="round"></path> <path d="M11 18H3" stroke="#fe2a2a" stroke-width="1.5" stroke-linecap="round"></path> <path d="M18.875 14.1183C20.5288 15.0732 21.3558 15.5506 21.4772 16.2394C21.5076 16.4118 21.5076 16.5881 21.4772 16.7604C21.3558 17.4492 20.5288 17.9266 18.875 18.8815C17.2212 19.8363 16.3942 20.3137 15.737 20.0745C15.5725 20.0147 15.4199 19.9265 15.2858 19.814C14.75 19.3644 14.75 18.4096 14.75 16.4999C14.75 14.5902 14.75 13.6354 15.2858 13.1858C15.4199 13.0733 15.5725 12.9852 15.737 12.9253C16.3942 12.6861 17.2212 13.1635 18.875 14.1183Z" stroke="#fe2a2a" stroke-width="1.5"></path> <path d="M3 6L13.5 6M20 6L17.75 6" stroke="#fe2a2a" stroke-width="1.5" stroke-linecap="round"></path> <path d="M20 10L9.5 10M3 10H5.25" stroke="#fe2a2a" stroke-width="1.5" stroke-linecap="round"></path> </g></svg><span>{name}</span></span>
-      </div>:<div className={styles.card} onClick={() => navigate("/watchlist/" + id)}>
-        <span><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M11 14L3 14" stroke="#fe2a2a" stroke-width="1.5" stroke-linecap="round"></path> <path d="M11 18H3" stroke="#fe2a2a" stroke-width="1.5" stroke-linecap="round"></path> <path d="M18.875 14.1183C20.5288 15.0732 21.3558 15.5506 21.4772 16.2394C21.5076 16.4118 21.5076 16.5881 21.4772 16.7604C21.3558 17.4492 20.5288 17.9266 18.875 18.8815C17.2212 19.8363 16.3942 20.3137 15.737 20.0745C15.5725 20.0147 15.4199 19.9265 15.2858 19.814C14.75 19.3644 14.75 18.4096 14.75 16.4999C14.75 14.5902 14.75 13.6354 15.2858 13.1858C15.4199 13.0733 15.5725 12.9852 15.737 12.9253C16.3942 12.6861 17.2212 13.1635 18.875 14.1183Z" stroke="#fe2a2a" stroke-width="1.5"></path> <path d="M3 6L13.5 6M20 6L17.75 6" stroke="#fe2a2a" stroke-width="1.5" stroke-linecap="round"></path> <path d="M20 10L9.5 10M3 10H5.25" stroke="#fe2a2a" stroke-width="1.5" stroke-linecap="round"></path> </g></svg><span>{name}</span></span>
-      </div>}
-      
+      {!isOdd ? (
+        <div
+          className={styles.card}
+          style={{backgroundImage: "linear-gradient(to right, #26222266, #ffffff51)"}}
+          onClick={() => navigate("/watchlist/" + id)}
+        >
+          <span>
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <path d="M11 14L3 14" stroke="#fe2a2a" stroke-width="1.5" stroke-linecap="round"></path>{" "}
+                <path d="M11 18H3" stroke="#fe2a2a" stroke-width="1.5" stroke-linecap="round"></path>{" "}
+                <path
+                  d="M18.875 14.1183C20.5288 15.0732 21.3558 15.5506 21.4772 16.2394C21.5076 16.4118 21.5076 16.5881 21.4772 16.7604C21.3558 17.4492 20.5288 17.9266 18.875 18.8815C17.2212 19.8363 16.3942 20.3137 15.737 20.0745C15.5725 20.0147 15.4199 19.9265 15.2858 19.814C14.75 19.3644 14.75 18.4096 14.75 16.4999C14.75 14.5902 14.75 13.6354 15.2858 13.1858C15.4199 13.0733 15.5725 12.9852 15.737 12.9253C16.3942 12.6861 17.2212 13.1635 18.875 14.1183Z"
+                  stroke="#fe2a2a"
+                  stroke-width="1.5"
+                ></path>{" "}
+                <path d="M3 6L13.5 6M20 6L17.75 6" stroke="#fe2a2a" stroke-width="1.5" stroke-linecap="round"></path>{" "}
+                <path d="M20 10L9.5 10M3 10H5.25" stroke="#fe2a2a" stroke-width="1.5" stroke-linecap="round"></path>{" "}
+              </g>
+            </svg>
+            <span>{name}</span>
+          </span>
+        </div>
+      ) : (
+        <div className={styles.card} onClick={() => navigate("/watchlist/" + id)}>
+          <span>
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <path d="M11 14L3 14" stroke="#fe2a2a" stroke-width="1.5" stroke-linecap="round"></path>{" "}
+                <path d="M11 18H3" stroke="#fe2a2a" stroke-width="1.5" stroke-linecap="round"></path>{" "}
+                <path
+                  d="M18.875 14.1183C20.5288 15.0732 21.3558 15.5506 21.4772 16.2394C21.5076 16.4118 21.5076 16.5881 21.4772 16.7604C21.3558 17.4492 20.5288 17.9266 18.875 18.8815C17.2212 19.8363 16.3942 20.3137 15.737 20.0745C15.5725 20.0147 15.4199 19.9265 15.2858 19.814C14.75 19.3644 14.75 18.4096 14.75 16.4999C14.75 14.5902 14.75 13.6354 15.2858 13.1858C15.4199 13.0733 15.5725 12.9852 15.737 12.9253C16.3942 12.6861 17.2212 13.1635 18.875 14.1183Z"
+                  stroke="#fe2a2a"
+                  stroke-width="1.5"
+                ></path>{" "}
+                <path d="M3 6L13.5 6M20 6L17.75 6" stroke="#fe2a2a" stroke-width="1.5" stroke-linecap="round"></path>{" "}
+                <path d="M20 10L9.5 10M3 10H5.25" stroke="#fe2a2a" stroke-width="1.5" stroke-linecap="round"></path>{" "}
+              </g>
+            </svg>
+            <span>{name}</span>
+          </span>
+        </div>
+      )}
+
       {/* <button className={styles.delete_icon} onClick={deleteWatchlist}>
     <img src={deleteIcon} alt="delete icon"  />
     </button> */}
