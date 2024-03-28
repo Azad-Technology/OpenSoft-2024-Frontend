5876cab85caecaf78cc319a1b2f686325ec62b50
Harsh Vardhan Gupta <harsh247@gmail.com>
2024-03-29 01:55:13 +0530

    fixed:1.UI when no favourites or watchlists2. improved UI of watchlist cards

--- src/Components/Watchlists/AllWatchlists.jsx
+++ src/Components/Watchlists/AllWatchlists.jsx
@@ -23,7 +23,7 @@ export const AllWatchlists = () => {
     <>
       {watchlists && watchlists.length > 0 && (
         <>
-          <h1>Watchlists</h1>
+          
           <div className={styles.slider}>
             <div className={styles.container}>
               <div className={styles.slider__movies} ref={scrollableDivRef}>
@@ -36,7 +36,7 @@ export const AllWatchlists = () => {
         </>
       )}
 
-      {(!watchlists || watchlists.length === 0) && <div className={styles.nothingToShow}>Nothing to show here</div>}
+      {(!watchlists || watchlists.length === 0) &&  <div className={styles.favorites_card}><div className={styles.nothingToShow}><div> <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M15 18.5L20 13.5M20 18.5L15 13.5" stroke="#ff2e2e" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path opacity="0.5" d="M21 6L3 6" stroke="#ff2e2e" stroke-width="1.5" stroke-linecap="round"></path> <path opacity="0.5" d="M21 10L3 10" stroke="#ff2e2e" stroke-width="1.5" stroke-linecap="round"></path> <path opacity="0.5" d="M11 14L3 14" stroke="#ff2e2e" stroke-width="1.5" stroke-linecap="round"></path> <path opacity="0.5" d="M11 18H3" stroke="#ff2e2e" stroke-width="1.5" stroke-linecap="round"></path> </g></svg></div><div>No watchlist created.</div></div></div>}
     </>
   );
 };
