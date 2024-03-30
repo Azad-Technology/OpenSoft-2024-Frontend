58e414dde2345f7dc9655716721363e14f68d9dc
arghyadhali123 <dhali.arghya2016@gmail.com>
2024-03-30 08:57:25 +0530

    lengthy comments truncate for mobile view, show more + comment card timing + comments profilepic

--- src/Components/CommentCard/CommentCard.jsx
+++ src/Components/CommentCard/CommentCard.jsx
@@ -39,7 +39,7 @@ function timeSince(date) {
 function CommentCard({username, profilePic, comment, movie, link, timestamp}) {
   const commentTime = new Date(timestamp);
   const timeAgo = timeSince(commentTime);
-  const maxLength = 120; // Adjust this value as needed
+  const maxLength = 100; // Adjust this value as needed
   const truncatedComment = truncateComment(comment, maxLength);
 
   return (
@@ -48,7 +48,7 @@ function CommentCard({username, profilePic, comment, movie, link, timestamp}) {
         <div className={styles.title}>
           {profilePic != "" && <img src={profilePic} alt="" className={styles.profilePic} />}
           <div>
-            <h3 className={styles.username}>{username}</h3>
+            <div className={styles.username}>{username}</div>
             <div className={styles.cardTime}>{timestamp != "" && timeAgo}</div>
           </div>
         </div>
